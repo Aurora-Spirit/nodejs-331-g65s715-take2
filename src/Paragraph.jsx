4eb347e3b83205bd6a15a5b5c071999f3735fb82
Aurora_Spirit <salacamder@gmail.com>
2025-11-14 21:50:06 -0700

    fixed github link after initial commit

--- src/Paragraph.jsx
+++ src/Paragraph.jsx
@@ -6,7 +6,7 @@ function Paragraph(props) {
     <>
         <div>
             <p>We will be using TheMealDB API for our project.  We will use this to create a recipe generator website.</p>
-            <a href="https://github.com/Aurora-Spirit/aurora-spirit.github.io">Github</a>
+            <a href="https://github.com/Aurora-Spirit/nodejs-331-g65s715-take2">Github</a>
         </div>
     </>
   )
